8a51ae4bb28c00dd95166fed17a0bd0823654128
abdmelissamerve <90768816+abdmelissamerve@users.noreply.github.com>
2023-04-08 22:22:26 +0300

    rename

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,92 +1,87 @@
-import type { ReactElement, ReactNode } from 'react';
+import type { ReactElement, ReactNode } from "react";
 
-import type { NextPage } from 'next';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import Router from 'next/router';
-import nProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-import ThemeProvider from 'src/theme/ThemeProvider';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import createEmotionCache from 'src/createEmotionCache';
-import { appWithTranslation } from 'next-i18next';
-import { SidebarProvider } from 'src/contexts/SidebarContext';
-import 'src/utils/chart';
-import { Provider as ReduxProvider } from 'react-redux';
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Router from "next/router";
+import nProgress from "nprogress";
+import "nprogress/nprogress.css";
+import ThemeProvider from "src/theme/ThemeProvider";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "src/createEmotionCache";
+import { appWithTranslation } from "next-i18next";
+import { SidebarProvider } from "src/contexts/SidebarContext";
+import "src/utils/chart";
+import { Provider as ReduxProvider } from "react-redux";
 
-import Loader from 'src/components/Loader';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import useScrollTop from 'src/hooks/useScrollTop';
-import { SnackbarProvider } from 'notistack';
-import { AuthConsumer, AuthProvider } from 'src/contexts/FirebaseAuthContext';
-import { AbilityContext } from '@/contexts/Can';
-import defineAbility from '@/utils/ability';
+import Loader from "src/components/Loader";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import useScrollTop from "src/hooks/useScrollTop";
+import { SnackbarProvider } from "notistack";
+import { AuthConsumer, AuthProvider } from "src/contexts/FirebaseAuthContext";
+import { AbilityContext } from "@/contexts/Can";
+import defineAbility from "@/utils/ability";
 
 const clientSideEmotionCache = createEmotionCache();
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
+    getLayout?: (page: ReactElement) => ReactNode;
 };
 
 interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-  Component: NextPageWithLayout;
+    emotionCache?: EmotionCache;
+    Component: NextPageWithLayout;
 }
 
 function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  const getLayout = Component.getLayout ?? ((page) => page);
-  useScrollTop();
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+    const getLayout = Component.getLayout ?? ((page) => page);
+    useScrollTop();
 
-  Router.events.on('routeChangeStart', nProgress.start);
-  Router.events.on('routeChangeError', nProgress.done);
-  Router.events.on('routeChangeComplete', nProgress.done);
+    Router.events.on("routeChangeStart", nProgress.start);
+    Router.events.on("routeChangeError", nProgress.done);
+    Router.events.on("routeChangeComplete", nProgress.done);
 
-  return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>Admintools.io</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-      </Head>
-      {/* <ReduxProvider store={store}> */}
-        <SidebarProvider>
-          <ThemeProvider>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <AuthProvider>
-                <SnackbarProvider
-                  maxSnack={6}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right'
-                  }}
-                >
-                  <CssBaseline />
-                  <AuthConsumer>
-                    {(auth) =>
-                      !auth.isInitialized ? (
-                        <Loader />
-                      ) : (
-                        <AbilityContext.Provider
-                          value={defineAbility(auth.user)}
-                        >
-                          {getLayout(<Component {...pageProps} />)}
-                        </AbilityContext.Provider>
-                      )
-                    }
-                  </AuthConsumer>
-                </SnackbarProvider>
-              </AuthProvider>
-            </LocalizationProvider>
-          </ThemeProvider>
-        </SidebarProvider>
-      {/* </ReduxProvider> */}
-    </CacheProvider>
-  );
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>CSMLFinal</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            </Head>
+            {/* <ReduxProvider store={store}> */}
+            <SidebarProvider>
+                <ThemeProvider>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <AuthProvider>
+                            <SnackbarProvider
+                                maxSnack={6}
+                                anchorOrigin={{
+                                    vertical: "bottom",
+                                    horizontal: "right",
+                                }}
+                            >
+                                <CssBaseline />
+                                <AuthConsumer>
+                                    {(auth) =>
+                                        !auth.isInitialized ? (
+                                            <Loader />
+                                        ) : (
+                                            <AbilityContext.Provider value={defineAbility(auth.user)}>
+                                                {getLayout(<Component {...pageProps} />)}
+                                            </AbilityContext.Provider>
+                                        )
+                                    }
+                                </AuthConsumer>
+                            </SnackbarProvider>
+                        </AuthProvider>
+                    </LocalizationProvider>
+                </ThemeProvider>
+            </SidebarProvider>
+            {/* </ReduxProvider> */}
+        </CacheProvider>
+    );
 }
 
 export default appWithTranslation(MyApp);
--- src/components/LogoSign/index.tsx
+++ src/components/LogoSign/index.tsx
@@ -1,23 +1,15 @@
-import {
-  Box,
-  Tooltip,
-  Badge,
-  TooltipProps,
-  tooltipClasses,
-  styled,
-  useTheme
-} from '@mui/material';
-import Link from 'src/components/Link';
-import { useTranslation } from 'react-i18next';
+import { Box, Tooltip, Badge, TooltipProps, tooltipClasses, styled, useTheme } from "@mui/material";
+import Link from "src/components/Link";
+import { useTranslation } from "react-i18next";
 
-const LogoWrapper = styled('div')(
-  () => `
+const LogoWrapper = styled("div")(
+    () => `
     text-align: center;
   `
 );
 
 const LogoLink = styled(Link)(
-  ({ theme }) => `
+    ({ theme }) => `
     color: ${theme.palette.text.primary};
     text-decoration: none;
     display: inline-block;
@@ -26,35 +18,34 @@ const LogoLink = styled(Link)(
 );
 
 const TooltipWrapper = styled(({ className, ...props }: TooltipProps) => (
-  <Tooltip {...props} classes={{ popper: className }} />
+    <Tooltip {...props} classes={{ popper: className }} />
 ))(({ theme }) => ({
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: theme.colors.alpha.trueWhite[100],
-    color: theme.palette.getContrastText(theme.colors.alpha.trueWhite[100]),
-    fontSize: theme.typography.pxToRem(12),
-    fontWeight: 'bold',
-    borderRadius: theme.general.borderRadiusSm,
-    boxShadow:
-      '0 .2rem .8rem rgba(7,9,25,.18), 0 .08rem .15rem rgba(7,9,25,.15)'
-  },
-  [`& .${tooltipClasses.arrow}`]: {
-    color: theme.colors.alpha.trueWhite[100]
-  }
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: theme.colors.alpha.trueWhite[100],
+        color: theme.palette.getContrastText(theme.colors.alpha.trueWhite[100]),
+        fontSize: theme.typography.pxToRem(12),
+        fontWeight: "bold",
+        borderRadius: theme.general.borderRadiusSm,
+        boxShadow: "0 .2rem .8rem rgba(7,9,25,.18), 0 .08rem .15rem rgba(7,9,25,.15)",
+    },
+    [`& .${tooltipClasses.arrow}`]: {
+        color: theme.colors.alpha.trueWhite[100],
+    },
 }));
 
 function Logo() {
-  const { t }: { t: any } = useTranslation();
-  const theme = useTheme();
+    const { t }: { t: any } = useTranslation();
+    const theme = useTheme();
 
-  return (
-    <TooltipWrapper title={t('AdminTools')} arrow>
-      <LogoWrapper>
-        <LogoLink href="/">
-          <img src="/static/images/logo/logo.svg" />
-        </LogoLink>
-      </LogoWrapper>
-    </TooltipWrapper>
-  );
+    return (
+        <TooltipWrapper title={t("CSML")} arrow>
+            <LogoWrapper>
+                <LogoLink href="/">
+                    <img src="/static/images/logo/logo.svg" />
+                </LogoLink>
+            </LogoWrapper>
+        </TooltipWrapper>
+    );
 }
 
 export default Logo;
