72a7d5297da4eb2c948bdeb3497ec31b9c0dc88a
abdmelissamerve <90768816+abdmelissamerve@users.noreply.github.com>
2023-06-16 18:02:35 +0300

    removed sentry

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const calendarTranspile = require('next-transpile-modules')([
 ]);
 
 const withImages = require('next-images');
-const { withSentryConfig } = require('@sentry/nextjs');
 
 const moduleExports = withImages(
   calendarTranspile({
@@ -26,16 +25,5 @@ const moduleExports = withImages(
   })
 );
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
 
-  silent: true // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = moduleExports
